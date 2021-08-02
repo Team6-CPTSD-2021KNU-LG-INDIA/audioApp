c80e736e23bbfb501d9e1348e90500f4881ad21f
unknown <jdh9797@knu.ac.kr>
2021-08-03 05:36:46 +0900

    shell update

--- LED_Server/shelltest.js
+++ LED_Server/shelltest.js
@@ -1,6 +1,11 @@
 const shell=require('shelljs');
 
-var test=exec('/usr/bin/luna-send -n 1 -f luna://com.webos.service.wifi/getNetworks \'{}\'',{async:true});
-test.stdout.on('returnValue',function(returnValue){
-    console.log(returnValue);
+var test=shell.exec('/usr/bin/luna-send -n 1 -f luna://com.webos.service.wifi/getNetworks \'{}\'',{async:true});
+test.stdout.on('test',function(data){
+    console.log(data);
+    //data는 json형태의 string
+    var obj=JSON.parse(data);
+    if(!obj.returnValue){
+        console.log(obj.returnValue);
+    }
 });
