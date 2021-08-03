12fee3e0a456bbf626cc61b46a6ccf368827824d
unknown <jdh9797@knu.ac.kr>
2021-08-03 16:39:32 +0900

    final

--- LED_Server/shelltest.js
+++ LED_Server/shelltest.js
@@ -5,22 +5,18 @@ const pin="gpio4";
 
 function init_gpio(){
     var open_param=`'{"pin":"${pin}"}'`;
-    var open_gpio=shell.exec(luna_command+'open '+open_param,{async:false});
-    open_gpio.stdout.on('data',function(data){
-        //console.log(data);
-        //data는 json형태의 string
-        var obj=JSON.parse(data);
+    shell.exec(luna_command+'open '+open_param,function(code,stdout,stderr){
+        var obj=JSON.parse(stdout);
         if(!obj.returnValue){
-            console.log(obj.returnValue);
+            console.log(stderr);
             shell.exit(1);
         }
     });
     var direction_param=`'{"pin":"${pin}","Direction":"outLow"}'`;
-    var set_direction=shell.exec(luna_command+'setDirection '+direction_param,{async:false});
-    set_direction.stdout.on('data',function(data){
-        var obj=JSON.parse(data);
+    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
+        var obj=JSON.parse(stdout);
         if(!obj.returnValue){
-            console.log(obj.errorText);
+            console.log(stderr);
             shell.exit(1);
         }
     });
@@ -28,31 +24,29 @@ function init_gpio(){
 
 function turn_on(){
     var direction_param=`'{"pin":"${pin}","Direction":"outHigh"}'`;
-    var set_direction=shell.exec(luna_command+'setDirection '+direction_param,{async:false});
-    set_direction.stdout.on('data',function(data){
-        var obj=JSON.parse(data);
+    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
+        var obj=JSON.parse(stdout);
         if(!obj.returnValue){
-            console.log(obj.errorText);
+            console.log(stderr);
             shell.exit(1);
         }
     });
 }
 function turn_off(){
     var direction_param=`'{"pin":"${pin}","Direction":"outLow"}'`;
-    var set_direction=shell.exec(luna_command+'setDirection '+direction_param,{async:false});
-    set_direction.stdout.on('data',function(data){
-        var obj=JSON.parse(data);
+    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
+        var obj=JSON.parse(stdout);
         if(!obj.returnValue){
-            console.log(obj.errorText);
+            console.log(stderr);
             shell.exit(1);
         }
     });
 }
+
 function light(){
     var get_param=`'{"pin":"${pin}"}'`;
-    var get_value=shell.exec(luna_command+'getValue '+get_param,{async:false});
-    get_value.stdout.on('data',function(data){
-        var obj=JSON.parse(data);
+    shell.exec(luna_command+'getValue '+get_param,function(code,stdout,stderr){
+        var obj=JSON.parse(stdout);
         if(obj.value=="low"){
             turn_on();
         }else{
