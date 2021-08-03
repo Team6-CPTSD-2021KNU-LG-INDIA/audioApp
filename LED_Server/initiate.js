6a38105e9e278533d169d34d53d506a226c1b98f
unknown <jdh9797@knu.ac.kr>
2021-08-03 17:29:26 +0900

    init

--- LED_Server/initiate.js
+++ LED_Server/initiate.js
@@ -4,18 +4,12 @@ const luna_command='/usr/bin/luna-send -n 1 -f luna://com.webos.service.peripher
 const pin="gpio4";
 
 function init_gpio(){
-    var get_param=`'{"pin":"${pin}"}'`;
-    shell.exec(luna_command+'getValue '+get_param,function(code,stdout,stderr){
+    var open_param=`'{"pin":"${pin}"}'`;
+    shell.exec(luna_command+'open '+open_param,function(code,stdout,stderr){
         var obj=JSON.parse(stdout);
         if(!obj.returnValue){
-            var open_param=`'{"pin":"${pin}"}'`;
-            shell.exec(luna_command+'open '+open_param,function(code,stdout,stderr){
-                var obj=JSON.parse(stdout);
-                if(!obj.returnValue){
-                    console.log(stderr);
-                    shell.exit(1);
-                }
-            });
+            console.log(stderr);
+            shell.exit(1);
         }
     });
     
