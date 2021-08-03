d17ec09793d657b095abec3cf89d8e2f65b5f180
unknown <jdh9797@knu.ac.kr>
2021-08-03 17:05:15 +0900

    logic

--- LED_Server/initiate.js
+++ LED_Server/initiate.js
@@ -29,38 +29,5 @@ function init_gpio(){
     });
 }
 
-function turn_on(){
-    var direction_param=`'{"pin":"${pin}","direction":"outHigh"}'`;
-    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
-        var obj=JSON.parse(stdout);
-        if(!obj.returnValue){
-            console.log(stderr);
-            shell.exit(1);
-        }
-    });
-}
-function turn_off(){
-    var direction_param=`'{"pin":"${pin}","direction":"outLow"}'`;
-    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
-        var obj=JSON.parse(stdout);
-        if(!obj.returnValue){
-            console.log(stderr);
-            shell.exit(1);
-        }
-    });
-}
-
-function light(){
-    var get_param=`'{"pin":"${pin}"}'`;
-    shell.exec(luna_command+'getValue '+get_param,function(code,stdout,stderr){
-        var obj=JSON.parse(stdout);
-        if(obj.value=="low"){
-            turn_on();
-        }else{
-            turn_off();
-        }
-    });
-}
 
 init_gpio();
-light();
--- LED_Server/switch.js
+++ LED_Server/switch.js
@@ -3,32 +3,6 @@ const shell=require('shelljs');
 const luna_command='/usr/bin/luna-send -n 1 -f luna://com.webos.service.peripheralmanager/gpio/'
 const pin="gpio4";
 
-function init_gpio(){
-    var get_param=`'{"pin":"${pin}"}'`;
-    shell.exec(luna_command+'getValue '+get_param,function(code,stdout,stderr){
-        var obj=JSON.parse(stdout);
-        if(!obj.returnValue){
-            var open_param=`'{"pin":"${pin}"}'`;
-            shell.exec(luna_command+'open '+open_param,function(code,stdout,stderr){
-                var obj=JSON.parse(stdout);
-                if(!obj.returnValue){
-                    console.log(stderr);
-                    shell.exit(1);
-                }
-            });
-        }
-    });
-    
-    var direction_param=`'{"pin":"${pin}","direction":"outLow"}'`;
-    shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
-        var obj=JSON.parse(stdout);
-        if(!obj.returnValue){
-            console.log(stderr);
-            shell.exit(1);
-        }
-    });
-}
-
 function turn_on(){
     var direction_param=`'{"pin":"${pin}","direction":"outHigh"}'`;
     shell.exec(luna_command+'setDirection '+direction_param,function(code,stdout,stderr){
@@ -62,5 +36,4 @@ function light(){
     });
 }
 
-init_gpio();
 light();
